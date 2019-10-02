08b61568e13e3008d703536b9167ef68a8254852
a dinosaur <nick@a-dinosaur.com>
2019-10-03 00:44:27 +1000

    add stub for loading vgz directly

--- neotools/autoextract.c
+++ neotools/autoextract.c
@@ -12,6 +12,14 @@ int main(int argc, char** argv)
 	if (!file)
 		return 1;
 
+	if (fgetc(file) == 0x1F && fgetc(file) == 0x8B)
+	{
+		printf("I'm a little gzip short and stout\n");
+		return 2;
+	}
+
+	fseek(file, 0, SEEK_SET);
+
 	int err = vgmExtractSamples(file);
 	fclose(file);
 	return err;
--- neotools/autoextract.sh
+++ neotools/autoextract.sh
@@ -5,16 +5,8 @@ FILE="$1"
 NAME="$(basename "$FILE")"
 WAVDIR="${NAME%%.*}"
 
-if [ "${NAME##*.}" = "vgz" ]; then
-	cp "$FILE" "temp.vgm.gz"
-	gzip -fd "temp.vgm.gz"
-	FILE="temp.vgm"
-fi
-
 ./neoadpcmextract "$FILE"
 mkdir -p "$WAVDIR"
 for I in smpa_*.pcm; do ./adpcm "$I" "$WAVDIR/${I%%.*}.wav"; done
 for I in smpb_*.pcm; do ./adpcmb -d "$I" "$WAVDIR/${I%%.*}.wav"; done
 find . -type f -name "*.pcm" -exec rm -f {} \;
-
-[ "$FILE" = "temp.vgm" ] && rm -f "temp.vgm"
