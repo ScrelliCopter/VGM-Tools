dbce8e5c296524439bf1e01fe21c7a877172ae64
a dinosaur <dino@a-dinosaur.com>
2023-03-21 14:28:36 +1100

    normalise macro formatting

--- common/util.h
+++ common/util.h
@@ -6,11 +6,11 @@
 #define CLAMP(X, A, B) (MIN(MAX((X), (A)), (B)))
 
 #if ( defined( __GNUC__ ) && ( __GNUC__ >= 4 ) ) || defined( __clang__ )
-  #define FORCE_INLINE inline __attribute__((always_inline))
+# define FORCE_INLINE inline __attribute__((always_inline))
 #elif defined( _MSC_VER )
-  #define FORCE_INLINE __forceinline
+# define FORCE_INLINE __forceinline
 #else
-  #define FORCE_INLINE inline
+# define FORCE_INLINE inline
 #endif
 
 #endif//COMMON_UTIL_H
